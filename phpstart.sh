1543664442e80431ea02f1e697c700e974ccd82b
Ueliton Alves Dos Santos <ueliton@disroot.org>
2024-10-25 18:14:35 -0300

    Update phpstart.sh

--- phpstart.sh
+++ phpstart.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 #upnpc -e 'Servidor Web' -r 8080 TCP
-# lsof -i :8443
-# openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.crt
+# lsof -i :8080
 # php -S localhost:8080 index.php
-php -S 100.85.145.112:8080 -t ~/furiadetitas/
-#php -S 192.168.0.9:8080 -t ~/furiadetitas.net/
+#php -S 100.85.145.112:8080 -t ~/furiadetitas/
+php -S localhost:8080 -t ~/furiadetitas/
 #php -S localhost:8443 -t ~/furiadetitas.net/ -d "openssl.cafile=server.crt" -d "openssl.local_cert=server.crt" -d "openssl.local_pk=server.key"
